9a820a456191f5109b7887acfcf251422755fba1
Luper Rouch <luper.rouch@gmail.com>
2015-01-28 22:01:55 +0100

    cli: renamed --check to --cond; checking that one of --cond or --settings is used

--- multiwait/cli.py
+++ multiwait/cli.py
@@ -10,14 +10,14 @@ from . import modules
 def main():
     # Parse command line
     parser = argparse.ArgumentParser(description='Wait for stuff to happen '
-            'before running a command.',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--settings', default='/etc/multiwait.yaml',
-            help='Path to the settings file.')
-    parser.add_argument('-c', '--check', metavar='CONDITION', action='append',
-            dest='checks', help='Check for a condition. Use the form '
-            'condition:arg1=value,arg2=value to pass parameters.')
-    parser.add_argument('command', help='The command to run.', nargs='?')
+            'before running a command.')
+    parser.add_argument('-s', '--settings', help='Path to the settings file.')
+    parser.add_argument('-c', '--cond', metavar='CONDITION', action='append',
+            dest='conditions', help='Check for a condition. Use the form '
+            'condition:arg1=value,arg2=value to pass parameters. Use '
+            '--cond multiple times to specify multiple conditions.')
+    parser.add_argument('command', nargs='?', help='The command to run. '
+            'If no command is given, simply print the check results.')
     parser.add_argument('args', nargs=argparse.REMAINDER,
             help='The command arguments.')
     options = parser.parse_args()
@@ -25,12 +25,12 @@ def main():
     # Discover condition modules
     modules.discover()
 
-    if options.checks:
+    if options.conditions:
         # Create default conditions from the check names passed in the command
         # line
         parsed_defaults = {}
         parsed_conditions = []
-        for check in options.checks:
+        for check in options.conditions:
             cond_name, _, args = check.partition(':')
             args_items = [x for x in args.split(',') if x.strip()]
             if args_items:
@@ -41,12 +41,15 @@ def main():
             else:
                 cond_args = {}
             parsed_conditions.append({cond_name: cond_args})
-    else:
+    elif options.settings:
         # Create conditions from settings
         with open(options.settings) as fp:
             settings = yaml.safe_load(fp)
         parsed_conditions = settings['conditions']
         parsed_defaults = settings.get('defaults', {})
+    else:
+        print 'You must give --settings or --cond'
+        sys.exit(1)
 
     conditions = modules.load_from_list(parsed_conditions, parsed_defaults)
 
